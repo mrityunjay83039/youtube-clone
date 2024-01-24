e6ecf1d9a06ff56276aabb3c7d34ba89629c5624
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-24 15:46:36 +0530

    setup fetchFromAPI function with api key in env file

---
+++ src/utils/fethFromAPI.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube-v31.p.rapidapi.com/search";
+
+const options = {
+  url: BASE_URL,
+  params: {
+    maxResults: "50",
+  },
+  headers: {
+    "X-RapidAPI-Key": process.env.REACT_APP_RAPID_KEY,
+    "X-RapidAPI-Host": "youtube-v31.p.rapidapi.com",
+  },
+};
+
+export const fetchFromAPI = async function (url) {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
