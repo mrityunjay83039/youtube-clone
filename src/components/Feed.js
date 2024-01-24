bd38f69eb5b6359490252db644eb45b7121d3a43
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-24 14:48:24 +0530

    created videos component, added category title on feed and created feed box

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import {Stack, Box, Typography} from '@mui/material'
-import {Sidebar} from '../components'
+import React from "react";
+import { Stack, Box, Typography } from "@mui/material";
+import { Sidebar, Videos } from "../components";
 
-const Feed = ()=>(
-  <Stack sx={{flexDirection:{sx:'column', md:'row'}}}>
-    <Box sx={{borderRight:'1px solid #3d3d3d', height:{sx:'auto', md:'92vh'}, px:{sx:0, md:2}}}>
-      <Sidebar/>
-      <Typography className='copyright' sx={{color:'#ffffff', mt:'1.5'}}>@copyright 2024 Youtube Clone</Typography>
+const Feed = () => (
+  <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+    <Box
+      sx={{
+        borderRight: "1px solid #3d3d3d",
+        height: { sx: "auto", md: "92vh" },
+        px: { sx: 0, md: 2 },
+      }}>
+      <Sidebar />
+      <Typography className="copyright" sx={{ color: "#ffffff", mt: "1.5" }}>
+        @copyright 2024 Youtube Clone
+      </Typography>
+    </Box>
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" sx={{ color: "white" }} mb={2}>
+        New <span style={{color:'#fc1503'}}>Video</span>
+      </Typography>
+      <Videos />
     </Box>
   </Stack>
-)
+);
 
-
-export default Feed
+export default Feed;
