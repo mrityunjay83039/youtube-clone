8be944bc81bbc00f51af6947ef9f9b441e1e46c1
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-24 13:23:49 +0530

    Searchbar desgin done, created sidebar file and added in feed

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import {Stack, Box, Typography} from '@mui/material'
+import {Sidebar} from '../components'
 
-function Feed() {
-  return (
-    <div>Feed</div>
-  )
-}
+const Feed = ()=>(
+  <Stack sx={{flexDirection:{sx:'column', md:'row'}}}>
+    <Box sx={{borderRight:'1px solid #3d3d3d', height:{sx:'auto', md:'92vh'}, px:{sx:0, md:2}}}>
+      <Sidebar/>
+      <Typography className='copyright' sx={{color:'#ffffff', mt:'1.5'}}>@copyright 2024 Youtube Clone</Typography>
+    </Box>
+  </Stack>
+)
+
 
 export default Feed
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,25 @@
 import React from 'react'
-
+import { useState } from 'react'
+import {Paper, IconButton} from '@mui/material'
+import {Search} from '@mui/icons-material'
 
 const SearchBar = () => (
-    <Paper>
-        <input type='text'/>
+    <Paper 
+    component="form"
+    onSubmit={()=>{}}
+    sx={{borderRadius:20,
+         border:'1px solid #e3e3e3',
+         pl:2,
+         boxShadow:'none',
+         mr: {sm:5}
+        }}>
+        <input type='text' className='search-bar' placeholder='Search...'
+    
+        />
+        <IconButton type='submit'
+        sx={{p:'10px', color:'red'}}>
+            <Search/>
+        </IconButton>
     </Paper>
 )
 
