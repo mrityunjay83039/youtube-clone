4450a49a414ab6d2769541d37cb3c5640e39c589
mrityunjay mishra <mrityunjay.83039@gmail.com>
2024-01-24 14:31:41 +0530

    sidebar design done with data

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from "react";
+import { Stack } from "@mui/material";
+import { categories } from "../utils/constants";
 
-function Sidebar() {
-  return (
-    <div>Sidebar</div>
-  )
-}
+const selectedCategory = "New";
 
-export default Sidebar
+const Sidebar = () => (
+  <Stack
+    sx={{
+      overflowY: "auto",
+      display: "flex",
+      flexDirection: { sx: "row", md: "column" },
+      height: { sx: "auto", md: "95%" },
+    }}>
+    {categories.map((category) => {
+      return (
+        <button
+          className="category-btn"
+          style={{
+            backgroundColor: category.name === selectedCategory && "#fc1503",
+            color: "#ffffff",
+          }}
+          key={category.name}>
+          <span
+            style={{
+              color: category.name === selectedCategory ? "white" : "red",
+              marginRight: '5px',
+            }}>
+            {category.icon}
+          </span>
+          <span style={{opacity: category.name === selectedCategory ? 1 : 0.8 }}>{category.name}</span>
+        </button>
+      );
+    })}
+  </Stack>
+);
+
+export default Sidebar;
